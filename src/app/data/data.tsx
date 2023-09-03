41d6ecf61095ab8581a137522a90c53390fc50cc
Baran Dogan <phscloq@gmail.com>
2023-09-03 23:51:52 +0300

    EDIT latest data link

--- src/app/data/data.tsx
+++ src/app/data/data.tsx
@@ -1,10 +1,11 @@
 
 export async function GetData(){
-    const url= 'https://barandogan.me/mock-data/mock-data.json';
+    const url = 'https://barandogan.me/mock-data/mock-data.json?' + Date.now();
     const res = await fetch(`${url}`);
     if(!res.ok){
         throw new Error('Failed to fetch data');
     }
     const data= await res.json();
+    console.log(data.length);
     return data;
 }
