46079cfd2e40be7b1f96365a5c6b34d6f23b753b
Baran Dogan <phscloq@gmail.com>
2024-01-30 13:48:22 +0300

    Update data source URL

--- src/app/data/data.tsx
+++ src/app/data/data.tsx
@@ -1,6 +1,6 @@
 
 export async function GetData(){
-    const url = 'https://barandogan.me/mock-data/mock-data.json?' + Date.now();
+    const url = 'https://phscloq.github.io/mock-data/mock-data.json?' + Date.now();
     const res = await fetch(`${url}`);
     if(!res.ok){
         throw new Error('Failed to fetch data');
