d462558c679096524fa25ef043603c7670a755b8
Baran <phscloq@gmail.com>
2024-12-01 23:19:18 +0300

    Refactor page component and update styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,12 @@ export default  function Home() {
 
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center ">
-      <h1 className=' text-5xl mb-12 font-bold'>Best Flight</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center max-w-4xl mx-auto">
+      <div className="bg-white p-6 w-full rounded-md shadow-lg">
+      <h1 className=' text-3xl mb-12 font-bold text-center'>Best Flight</h1>
       <Search
-    
       />
+      </div>
      
      
       
