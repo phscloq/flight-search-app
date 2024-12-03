05fdf275b97a79cf3ad26cf015398bab48117b9e
Baran <phscloq@gmail.com>
2024-12-03 19:17:16 +0300

    Refactor flights page component and update styling

--- src/app/flights/page.tsx
+++ src/app/flights/page.tsx
@@ -21,35 +21,41 @@ if(isLoading){
 
   return (
     <main className="flex min-h-screen flex-column justify-center ">    
-    <div className='w-full pt-10 sm:max-w-2xl'>
-        <h1 className='text-center mb-12 font-bold text-2xl'>Available Flights</h1>
-        <Link className='font-bold hover:underline' href='/'>{'<- Go back'}</Link>
-        <div className='flex items-center justify-between mb-4 px-4'>
-        <h3 className=' font-medium'>Sort by:</h3>
-        
+      <div className='w-full pt-10 sm:max-w-2xl'>
+        <h1 className='text-center mb-12 font-bold text-2xl dark:text-white'>Available Flights</h1>
+        <Link className='font-bold hover:underline dark:text-white' href='/'>{'<- Go back'}</Link>
+        <div className='flex items-center justify-between mb-4 px-4 dark:text-white'>
+          <h3 className=' font-medium'>Sort by:</h3>
         </div>
-      
-       <SortButtons />
+        
+        <SortButtons />
         <FlightsList 
         matchedFlights={filteredWDate}
         />
-           <div className=' px-4 mb-4 '> 
-                  <div className='p-4 bg-white  text-center w-full rounded shadow'>
-                   {filteredWDate.length <=0 ?   <><p>There is no flights based on your search.</p></>: <><p>There is no additional flights matching the date you are searching.</p>
-                      <p>Click the More button to see more flights on the same route </p></>                   
-                     }
-                     
-                  
-                    </div>
-            </div>
-          <div className={`flex justify-center ${filteredWDate.length ===0 ? 'hidden': ''} mb-2`}>
-              <button className='px-4 py-2 rounded bg-blue-600 text-white 
-              shadow hover:bg-blue-700' onClick={()=>handleToggleFlights()}>{showAllFlights ? 'Close': 'More'} </button>
+        
+        <div className=' px-4 mb-4 '> 
+          <div className='p-4 bg-white  text-center w-full rounded shadow'>
+            {filteredWDate.length <=0 ? 
+            <p>There is no flights based on your search.</p>
+            : 
+            <>
+              <p>There is no additional flights matching the date you are searching.</p>
+              <p>Click the More button to see more flights on the same route </p>
+            </>                   
+              }
+            
           </div>
-
+        </div>
+        
+        <div className={`flex justify-center ${filteredWDate.length ===0 ? 'hidden': ''} mb-2`}>
+          <button className='px-4 py-2 rounded bg-blue-600 text-white 
+          shadow hover:bg-blue-700' onClick={()=>handleToggleFlights()}>
+          {showAllFlights ? 'Close': 'More'} 
+          </button>
+        </div>
         {showAllFlights ? <FlightsList matchedFlights={filteredWRoute} /> : null}
         {returnFlights && searchParams.isRoundTrip ? <div className='px-4'>
-        <h1 className=' mb-2 font-bold text-xl'> Return Flights:</h1>
+        <h1 className=' mb-2 font-bold text-xl dark:text-white'> Return Flights:</h1>
 
         </div> : null}
         
