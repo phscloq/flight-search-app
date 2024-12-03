c0a06c6a54cdd5f2637a51247ed538fab2381aba
Baran <phscloq@gmail.com>
2024-12-03 19:18:25 +0300

    Update page.tsx
    Added AvailableFlights and Footer components to the Home page. Updated styling for dark mode.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
+import AvailableFlights from "./components/available-flights"
+import Footer from "./components/footer"
 import Search from "./components/search"
 export default  function Home() {
 
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center max-w-4xl mx-auto">
-      <div className="bg-white p-6 w-full rounded-md shadow-lg">
-      <h1 className=' text-3xl mb-12 font-bold text-center'>Best Flight</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center max-w-4xl mx-auto dark:text-white">
+      <AvailableFlights />
+      <div className="bg-white dark:bg-gray-800 p-6 w-full rounded-md shadow-lg">
+      <h1 className=' text-2xl mb-12 font-bold text-center '>Flight Search</h1>
       <Search
       />
       </div>
-     
+      <Footer />
      
       
     </main>
