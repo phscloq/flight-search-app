fa6eb453dfdccfee07dbc5041b216a663134f10d
Baran <phscloq@gmail.com>
2024-12-03 19:21:05 +0300

    Update data.tsx
    Removed `+ Date.now()` from the url and console.log for data length.

--- src/app/data/data.tsx
+++ src/app/data/data.tsx
@@ -1,11 +1,11 @@
 
 export async function GetData(){
-    const url = 'https://phscloq.github.io/mock-data/mock-data.json?' + Date.now();
+    const url = 'https://phscloq.github.io/mock-data/mock-data.json?';
     const res = await fetch(`${url}`);
     if(!res.ok){
         throw new Error('Failed to fetch data');
     }
     const data= await res.json();
-    console.log(data.length);
+   
     return data;
 }
