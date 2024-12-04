81196c8fc35ea9a366b58fbd07348bfee4c03ca8
Baran <phscloq@gmail.com>
2024-12-05 00:13:18 +0300

    Refactor page.tsx
    Refactored layout for better spacing and hierarchy: added padding to `<main>` element, moved 'Flight Search' heading outside of the card, and adjusted Search component structure. Updated styles for a cleaner and more consistent appearance.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ export default  function Home() {
 
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center max-w-4xl mx-auto dark:text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center max-w-4xl mx-auto p-4 sm:p-6 dark:text-white">
+      <h1 className=' text-2xl  font-bold text-center '>Flight Search</h1>
       <AvailableFlights />
-      <div className="bg-white dark:bg-gray-800 p-6 w-full rounded-md shadow-lg">
-      <h1 className=' text-2xl mb-12 font-bold text-center '>Flight Search</h1>
-      <Search
-      />
+      <div className="bg-white dark:bg-gray-800   w-full  rounded-md shadow-lg">
+        <Search
+        />
       </div>
       <Footer />
      
