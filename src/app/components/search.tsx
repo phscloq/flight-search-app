83f7aa51a97b2c12933dab293307446ec157d549
Baran <phscloq@gmail.com>
2024-12-05 00:16:18 +0300

    Refactor search.tsx
    Refactored search form component: improved layout structure. Imported FlightDateInputs and CityInputs components and removed them as separate functions.

--- src/app/components/search.tsx
+++ src/app/components/search.tsx
@@ -7,6 +7,8 @@ import { useSearchContext } from "../lib/SearchContext";
 /* import { handleDropdown } from "../lib/handleDropdown";
  */import Error from "./error";
 import {  useRouter } from "next/navigation";
+import CityInputs from "./city-inputs";
+import FlightDateInputs from "./flight-date-inputs";
 
 type InputsErrors ={
   departureCity: boolean,
@@ -80,13 +82,7 @@ export default function Search(){
       validateSearchParams();
     }, [searchParams]); */
 
-
-//today's date:
-const today= new Date();
-const year= today.getFullYear();
-const month = String(today.getMonth()+1).padStart(2, '0');
-const day = String(today.getDate()).padStart(2, '0');
-const formattedDate = `${year}-${month}-${day}`;
+    
 
 
 const handleSubmit = (e: React.FormEvent<HTMLFormElement>)=>{
@@ -113,16 +109,14 @@ const handleSubmit = (e: React.FormEvent<HTMLFormElement>)=>{
 }
 
     return(
-        <div className='  '>
+        <div className='p-6  w-full'>
           <Error error={error} />
-          
           <TicketType />
-          <form onSubmit={handleSubmit}>
-            <CityInputs inputErrors={inputErrors} />
-            <FlightDateInputs inputErrors={inputErrors} />  
+          <form onSubmit={handleSubmit} className="w-full ">
+              <CityInputs inputErrors={inputErrors} />
+              <FlightDateInputs inputErrors={inputErrors} />  
               <button className='w-full py-2 text-sm  text-white bg-blue-600 rounded-md font-semibold tracking-wide '
-              type="submit" 
-              >
+              type="submit">
                 Search flights
               </button>
           </form>
@@ -154,128 +148,6 @@ function TicketType(){
 }
 
 
-function FlightDateInputs({inputErrors}:{inputErrors: InputsErrors}){
-  const {searchParams, setSearchParams} = useSearchContext();
-  
-  const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>, departure:boolean) => {
-    const selectedDate = new Date(e.target.value);
-    if(departure){
-      setSearchParams((prevParams) => ({ ...prevParams, departureDate: selectedDate }));
-    }else{
-      setSearchParams((prevParams) => ({ ...prevParams, returnDate: selectedDate }));
-    }
-  
-  
-  };
 
 
-  return(
-    <div className='flex  w-full  mb-8'>
-      <div className='w-1/2 pr-2'>
-      <label className=' dark:text-white  text-sm font-medium leading-none' htmlFor="departureDate">Departure Date</label>
-        <input className='text-black w-full py-2  px-2 rounded-lg 
-        border    focus:ring-2 ring-blue-600 ring-offset-2 focus:outline-none mt-2 text-sm' 
-        type='date'
-        value={searchParams.departureDate ? searchParams.departureDate.toISOString().split('T')[0]: ''}
-        onChange={(e)=>handleDateChange(e, true)}
-        placeholder="Departure Date"
-        required
-        //default value olarak bugünün tarihi veririz bölyece garip bir şey gözükmez ve focusa girdiği gibi calendar açılsın
-        ></input>
-      </div>
-      <div className='w-1/2 pl-2'>
-      <label className=' dark:text-white  text-sm font-medium leading-none' htmlFor="returnDate">Return Date</label>
-        <input className={`text-black ${inputErrors.arrivalDate ? 'border-red-600' : 'border-gray-300' } w-full py-2  px-2 rounded-lg 
-          border focus:ring-2 ring-blue-600 ring-offset-2 focus:outline-none  text-sm mt-2`} 
-        type='date' disabled={!searchParams.isRoundTrip}
-        value={searchParams.returnDate ? searchParams.returnDate.toISOString().split('T')[0]: ''}
-        onChange={(e)=>handleDateChange(e, false)}
-        min={searchParams.departureDate?.toISOString().split('T')[0]}
-        required
-        ></input>
-      </div>
-    </div>
-    
-  )
-}
 
-
-function CityInputs({inputErrors}:{inputErrors: InputsErrors}){
-
-  const {searchParams, setSearchParams, cities, showDepartureCityList, showArrivalCityList, setShowArrival, setShowDepart} = useSearchContext();
-
-  const handleDropdown = (departure: boolean) => {
-    if (departure) {
-      setShowDepart(true);
-      setShowArrival(false);
-    } else {
-      setShowDepart(false);
-      setShowArrival(true);
-    }
-  };
-
-  const handleCityChange = (city: string, departure: boolean) => {
-    if(departure){
-      setSearchParams((prevParams) => ({ ...prevParams, departureCity: city }));
-    }else{
-      setSearchParams((prevParams) => ({ ...prevParams, arrivalCity: city }));
-      
-    }
-  }
-
-  const handleCitySelect = (city: string, departure: boolean) => {
-    if(departure){
-      setSearchParams((prevParams) => ({ ...prevParams, departureCity: city }));
-      setShowDepart(false);
-    }else{
-      setSearchParams((prevParams) => ({ ...prevParams, arrivalCity: city }));
-        setShowArrival(false);
-    }
-  }
-
-  return (
-    <div className='flex mb-8 w-full gap-4'>
-      <div className='w-1/2'>
-        <label className=' dark:text-white  text-sm font-medium leading-none' htmlFor="departureCity">Departure City</label>
-        <input className={`text-black ${inputErrors.departureCity ? 'border-red-600' : 'border-gray-300'} w-full py-2  px-2 rounded-lg 
-        border focus:ring-2 ring-blue-600 ring-offset-2 focus:outline-none mt-2 text-sm `}
-        placeholder='Enter departure city'
-        value={searchParams.departureCity}
-        onChange={(e)=>handleCityChange(e.target.value, true)}
-        onClick={()=>handleDropdown(true)}
-        id="departureCity"
-        name="departureCity"
-        type="text"
-        required
-        ></input>
-      <Cities 
-      cities={cities}
-      SearchedCity={searchParams.departureCity}
-      show={showDepartureCityList}
-      onSelect={(city:any)=>handleCitySelect(city, true)}
-      />
-      </div> 
-      <div className='w-1/2'>
-        <label className=' dark:text-white text-sm font-medium leading-none mb-4' htmlFor="arrivalCity">Arrival City</label>
-        <input className={`text-black ${inputErrors.arrivalCity ? 'border-red-600' : 'border-gray-300' } w-full py-2  px-2 rounded-lg 
-        border focus:ring-2 ring-blue-600 ring-offset-2 focus:outline-none mt-2 text-sm `}
-        placeholder='Enter arrival city'
-        value={searchParams.arrivalCity}
-        onChange={(e)=>handleCityChange(e.target.value, false)}
-        onClick={()=>handleDropdown(false)}
-        id="arrivalCity"
-        name="arrivalCity"
-        type="text"
-        required
-        ></input>
-        <Cities 
-      cities={cities}
-      SearchedCity={searchParams.arrivalCity}
-      show={showArrivalCityList}
-      onSelect={(city:any)=>handleCitySelect(city, false)}
-
-      />
-      </div> 
-</div>
-  )
-}
