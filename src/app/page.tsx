3095bed52914779af4eab24d0f60d88dd47848a7
Baran <phscloq@gmail.com>
2024-12-06 11:47:20 +0300

    Refactor Home Page
    Refactored Home Page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,13 @@ export default  function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center max-w-4xl mx-auto p-4 sm:p-6 dark:text-white">
-      <h1 className=' text-2xl  font-bold text-center '>Flight Search</h1>
+      <h2 className="text-xl font-bold mb-4 text-center">Available Flights </h2>
       <AvailableFlights />
-      <div className="bg-white dark:bg-gray-800   w-full  rounded-md shadow-lg">
-        <Search
-        />
+      <div className="bg-white dark:bg-gray-800   w-full  rounded-md shadow-lg pt-4">
+        <h1 className=' text-2xl font-bold text-center '>Flight Search</h1>
+        <Search/>
       </div>
       <Footer />
-     
-      
     </main>
   )
 }
