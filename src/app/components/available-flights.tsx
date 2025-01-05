ac4bd20b78507d65b1ddcb2e4d468dc8c8c78d89
Baran <phscloq@gmail.com>
2025-01-05 19:17:09 +0300

    Refactor AvailableFlights component layout.

--- src/app/components/available-flights.tsx
+++ src/app/components/available-flights.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { formatDate } from "../flights/components/formatTimeDate";
-import FilterFlights from "../lib/filterFlights";
 import { useSearchContext } from "../lib/SearchContext";
 
 export default function AvailableFlights(){
@@ -16,34 +15,35 @@ export default function AvailableFlights(){
         }))
       }
       
-      const flightsArr = generateFlights(40) */
-      
+      const flightsArr = generateFlights(1) */
     return(
-        <div className="mb-8 mx-auto max-w-4xl w-full overflow-x-hidden flex relative slider ">
-            <ul className="animate-infinite-scroll flex gap-8  hover:pause h-36  items-center">
-                {[...flights, ...flights].map((flight, index) => (
-                <li
-                    key={index}
-                    className={` w-48  cursor-default  sm:w-[300px] h-[100px] bg-white rounded-lg  p-2 sm:p-4 text-black  hover:scale-110 hover:shadow-lg transition-all duration-300`}
-                  
-                >
-                    <p className=" font-medium sm:text-lg sm:font-semibold">
-                    {flight.departure_city} to {flight.arrival_city}
-                    </p>
-                    <p className="text-sm">
-                    Date: {formatDate(flight.departure_time)}
-                    </p>
-                    <p className="text-sm">
-                    Available Seats: {Math.floor(Math.random() * 10)+1}
-                    </p>
-                    {/* <p>
-                        {index}
-                    </p> */}
+        <div className=" mb-8 mx-auto max-w-4xl w-full  flex relative slider ">
+         
+              <ul className=" animate-infinite-scroll flex gap-8  hover:pause h-36  items-center"
+              >
+                  {[...flights, ...flights].map((flight, index) => (
+                  <li
+                      key={index}
+                      className={`flex-shrink-0 w-48  cursor-default  sm:w-[300px] h-[100px] bg-white rounded-lg  p-2 sm:p-4 text-black  hover:scale-110 hover:shadow-lg transition-all duration-300`}>
+                      <p className=" font-medium sm:text-lg sm:font-semibold">
+                      {flight.departure_city} to {flight.arrival_city}
+                      </p>
+                      <p className="text-sm">
+                      Date: {formatDate(flight.departure_time)}
+                      </p>
+                      <p className="text-sm">
+                      Available Seats: {Math.floor(Math.random() * 10)+1}
+                      </p>
+                      {/* <p>
+                          {index}
+                      </p> */}
 
 
-                </li>
-                ))}
-            </ul>
+                  </li>
+                  ))}
+              </ul>
           </div>
     )
 }
+
+
