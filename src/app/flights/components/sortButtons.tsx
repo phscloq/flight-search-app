513a2946410d06ef3e334b484d73d9ef64f3616c
Baran <phscloq@gmail.com>
2025-01-05 19:24:03 +0300

    Fix sort button for arrival time: update class condition to match correct sort type

--- src/app/flights/components/sortButtons.tsx
+++ src/app/flights/components/sortButtons.tsx
@@ -4,7 +4,7 @@ export default function SortButtons(){
     return (
         <div className='flex mb-4  flex-wrap gap-2'>
         <button className={`${sortType === 'departure_time' ? '!bg-blue-600 text-white':''} px-4 py-2 rounded bg-white   shadow hover:bg-blue-700 hover:text-white`} onClick={() => handleSortType('departure_time')}>Departure</button>
-        <button className={`${sortType === 'arrival' ? '!bg-blue-600 text-white':''} px-4 py-2 rounded bg-white   shadow hover:bg-blue-700 hover:text-white`} onClick={() => handleSortType('arrival_time')}>Arrival</button>
+        <button className={`${sortType === 'arrival_time' ? '!bg-blue-600 text-white':''} px-4 py-2 rounded bg-white   shadow hover:bg-blue-700 hover:text-white`} onClick={() => handleSortType('arrival_time')}>Arrival</button>
         <button className={`${sortType === 'duration' ? '!bg-blue-600 text-white':''} px-4 py-2 rounded bg-white   shadow hover:bg-blue-700 hover:text-white`} onClick={() => handleSortType('duration')}>Duration</button>
         <button className={`${sortType === 'price' ? '!bg-blue-600 text-white':''} px-4 py-2 rounded bg-white   shadow hover:bg-blue-700 hover:text-white`} onClick={() => handleSortType('price')}>$ Price</button>
       </div>
